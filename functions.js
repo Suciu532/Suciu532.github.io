38c99b60566fe76ed0d2a330ed02f6663ffbcf20
Suciu Tudor <suciu532@gmail.com>
2021-07-09 17:38:44 +0300

    modificare functii show/hide+css

--- functions.js
+++ functions.js
@@ -1,22 +1,43 @@
-function hidePage(id) {
+function hide(id) {
     document.getElementById(id).style.display = 'none';
 }
 
-function showPage(id) {
+function highlight(el) {
+    el.style.borderColor = '#00df00';
+    setTimeout(function () {
+        el.style.borderColor = '#55df44';
+    }, 100);
+    setTimeout(function () {
+        el.style.borderColor = '#99df88';
+    }, 200);
+    setTimeout(function () {
+        el.style.borderColor = '#e0dfdc';
+        el.style.borderColor = '';
+    }, 300);
+}
+
+function show(id) {
     document.getElementById(id).style.display = 'block';
+    var page = document.getElementById(id);
+    if (page) {
+        page.style.display = 'block';
+        highlight(page);
+    } else {
+        console.warn("pagina cu id-ul %o nu exista", id);
+    }
 }
 
 function hideAllPages() {
     var pages = Array.from(document.getElementsByClassName('page'));
 
     pages.forEach(function (page) {
-        hidePage(page.id)
+        hide(page.id)
     });
 }
 
-function show(id) {
+function showPage(id) {
     hideAllPages();
-    showPage(id);
+    show(id);
 }
 
-show('skills');
+show('home');
