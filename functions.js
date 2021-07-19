8214bcd148bf49fb4caee54c481d6c9dc099c43f
Suciu Tudor <suciu532@gmail.com>
2021-07-19 20:15:37 +0300

    simplified return in arrow functions

--- functions.js
+++ functions.js
@@ -33,9 +33,7 @@ function show(id) {
 
 function hideAllPages() {
     const pages = Array.from(document.getElementsByClassName('page'));
-    pages.forEach(page => {
-        hide(page.id)
-    });
+    pages.forEach(page => hide(page.id));
 }
 
 function showPage(id) {
@@ -86,10 +84,10 @@ function sortSkillsByEndorsments(a, b) {
     return b.endorsements - a.endorsements;
 }
 
-fetch("data/skills.json").then((response) => {
-    return response.json();
-}).then(skills => {
-    skills.sort(sortSkillsByEndorsments);
-    window, skills = skills;
-    showSkills(skills);
-})
+fetch("data/skills.json")
+    .then(response => response.json())
+    .then(skills => {
+        skills.sort(sortSkillsByEndorsments);
+        window.skills = skills;
+        showSkills(skills);
+    })
